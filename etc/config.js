8da2ab99dc69b340389c70d75196bbbc9725246b
Igor Alekseenko <fastedx@gmail.com>
2018-04-25 22:51:46 +0300

    additional init commit

---
+++ etc/config.js
@@ -0,0 +1,7 @@
+
+const apiurls = {
+    basicUrl : '/apiv1/getdata',
+    sliderUrl: '/apiv1/getsliders',
+    servicesUrl: '/apiv1/getservices'
+}
+module.exports = urls;
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const express        = require('express');
-const bodyParser     = require('body-parser');
+const server = require('express')();
+const jsonParser = require('body-parser');
+const ulrs = require('./etc/config');
 
-const app = express();
+
 const port = 8000;
 
-
+//using body parser middleware via new express 4.x rules see more at  http://expressjs.com/ru/4x/api.html#req
+server.use(jsonParser.json()); // application/json
+server.use(jsonParser.urlencoded({ extended: false })); /// x-form-ulr-encoded
+/////////////////////////////////////////////
 
 async function read(){
 	await console.log('read async');
 }
 
+const test = () =>{
+	console.log('test');
+}
 
-
-app.listen(port,()=>{
-	read();
+server.listen(port,()=>{
 	console.log('server started on port '+port);
+})
+
+
+server.post(apiurls.basicUrl,function(req,res){
+	res.json({type:"ok",status:"200"});
 })
 
